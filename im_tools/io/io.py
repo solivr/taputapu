25cb08c92d6dc0123e2ccd21cfaccf80b1191873
soliveir <sofia.oliveiraares@epfl.ch>
2018-08-27 10:41:52 +0200

    get image shape without loading it

---
+++ im_tools/io/io.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+__author__ = "solivr"
+__license__ = "GPL"
+
+from PIL import Image
+from typing import Tuple
+
+
+def get_image_size_without_loading(filename: str) -> Tuple[int, int]:
+    """
+    Gives the shape (h,w) of the image without loading its content into memory
+    :param filename: filename of the image
+    :return: (H, W)
+    """
+    image = Image.open(filename)
+    shape = image.size()
+    image.close()
+    return shape
